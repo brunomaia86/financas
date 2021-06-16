34ff6aab6ae145e942bc49e45c1bc635ac01549d
Bruno <Bruno@host.docker.internal>
2021-06-16 00:06:34 -0300

    implmentacao do put dominio origem

--- src/main/java/com/brunomaia/financeiro/api/resource/DominioOrigemResource.java
+++ src/main/java/com/brunomaia/financeiro/api/resource/DominioOrigemResource.java
@@ -5,16 +5,20 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -62,6 +66,21 @@ public class DominioOrigemResource {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long codigo) {
 		dominioOrigemRepository.deleteById(codigo);
+	}
+	
+	@PutMapping("/codigo")
+	public ResponseEntity<DominioOrigem> atualizar(@PathVariable Long codigo, @Valid @RequestBody DominioOrigem dominioParam){
+		DominioOrigem dominioOrigem = this.dominioOrigemRepository.findById(codigo)
+				.orElseThrow(() -> new EmptyResultDataAccessException(1));
+		
+//		if(dominioOrigem == null) {
+//			throw new EmptyResultDataAccessException(1);
+//		}
+		
+		BeanUtils.copyProperties(dominioParam, dominioOrigem, "codigo");
+		
+		this.dominioOrigemRepository.save(dominioOrigem);
+		return ResponseEntity.ok(dominioOrigem);
 	}
 	
 }
