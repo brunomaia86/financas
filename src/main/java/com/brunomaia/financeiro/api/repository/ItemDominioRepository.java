e5c62bd9647c20511274df439247127978749253
Bruno <Bruno@host.docker.internal>
2021-06-30 19:03:50 -0300

    exclusao dominios dominio e item de dominios

--- src/main/java/com/brunomaia/financeiro/api/repository/ItemDominioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.brunomaia.financeiro.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.brunomaia.financeiro.api.modelo.ItemDominio;
-
-public interface ItemDominioRepository extends JpaRepository<ItemDominio, Long> {
-
-}
--- src/main/resources/db/migration/V00002__criar_movimentacao_financeira.sql
+++ src/main/resources/db/migration/V00002__criar_movimentacao_financeira.sql
@@ -3,6 +3,7 @@ create table movimentacao_financeira (
 	codigo_origem bigint not null, 
 	referencia date not null, 
 	registro datetime not null, 
-	valor decimal(19,2) not null, 
+	valor decimal(19,2) not null,
+	FOREIGN KEY (codigo_origem) REFERENCES dominio_origem(codigo),
 	primary key (codigo)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
